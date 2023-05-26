299455d9044caf30d82c063910b3393d7374bced
AnkushSinghGitHubAccount <134660991+AnkushSinghGitHubAccount@users.noreply.github.com>
2023-05-25 19:11:53 -0600

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Manu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie ')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie ')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast ' )
 
