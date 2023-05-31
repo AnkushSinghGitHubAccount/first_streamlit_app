da2af8f2ae6855d9b0b56ee324b88633848c1f1b
AnkushSinghGitHubAccount <134660991+AnkushSinghGitHubAccount@users.noreply.github.com>
2023-05-30 20:50:33 -0600

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -33,12 +33,12 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruity_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 
 
